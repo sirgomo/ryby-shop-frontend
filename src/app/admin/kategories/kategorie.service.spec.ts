3508e3cf43b59db24f506ce8b97b67b93df57aa5
Warczachowski <wlodek_w@yahoo.de>
2023-07-06 19:26:21 +0200

    work on tests

--- src/app/admin/kategories/kategorie.service.spec.ts
+++ src/app/admin/kategories/kategorie.service.spec.ts
@@ -1,16 +1,198 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { KategorieService } from './kategorie.service';
+import { ErrorService } from 'src/app/error/error.service';
+import { iKategorie } from 'src/app/model/iKategorie';
+import { environment } from 'src/environments/environment';
 
 describe('KategorieService', () => {
   let service: KategorieService;
+  let httpMock: HttpTestingController;
+  let snackbarSpy: jest.MockInstance<MatSnackBarRef<TextOnlySnackBar>, [string, string?, MatSnackBarConfig<any>?]>;
+  let errorServiceSpy: jest.SpyInstance;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const snackbarSpyObj = jest.fn();
+    const errorServiceSpyObj = jest.spyOn(ErrorService.prototype, 'newMessage').mockImplementation(() => {});
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        KategorieService,
+        { provide: MatSnackBar, useValue: { open: snackbarSpyObj } },
+        { provide: ErrorService, useValue: errorServiceSpyObj }
+      ]
+    });
+
     service = TestBed.inject(KategorieService);
+    httpMock = TestBed.inject(HttpTestingController);
+    snackbarSpy = snackbarSpyObj;
+    errorServiceSpy = errorServiceSpyObj;
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create a category', () => {
+    const categoryData = { name: 'Category 1' };
+    const expectedResponse: iKategorie = { id: 1, parent_id: null, name: 'Category 1', products: [] };
+
+    service.createCategory(categoryData).subscribe((result) => {
+      expect(result).toEqual([expectedResponse]);
+      expect(snackbarSpy).toHaveBeenCalledWith('Kategorie erstellt', 'ok', { duration: 1000 });
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie`);
+    expect(req.request.method).toBe('POST');
+    req.flush(expectedResponse);
+  });
+
+  it('should handle error when creating a category', () => {
+    const categoryData = { name: 'Category 1' };
+    const errorMessage = 'Error creating category';
+
+    service.createCategory(categoryData).subscribe((result) => {
+      expect(result).toEqual([]);
+      expect(errorServiceSpy).toHaveBeenCalledWith(errorMessage);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie`);
+    expect(req.request.method).toBe('POST');
+    req.flush({ message: errorMessage }, { status: 500, statusText: 'Internal Server Error' });
+  });
+
+  it('should get a category by id', () => {
+    const categoryId = 1;
+    const expectedResponse: iKategorie = { id: 1, parent_id: null, name: 'Category 1', products: [] };
+
+    service.getCategoryById(categoryId).subscribe((result) => {
+      expect(result).toEqual(expectedResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
+  });
+
+  it('should get all categories', () => {
+    const expectedResponse: iKategorie[] = [
+      { id: 1, parent_id: null, name: 'Category 1', products: [] },
+      { id: 2, parent_id: null, name: 'Category 2', products: [] }
+    ];
+
+    service.getAllCategories().subscribe((result) => {
+      expect(result).toEqual(expectedResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie`);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
+  });
+
+  it('should handle error when getting all categories', () => {
+    const errorMessage = 'Error getting categories';
+
+    service.getAllCategories().subscribe((result) => {
+      expect(result).toEqual([]);
+      expect(errorServiceSpy).toHaveBeenCalledWith('Es gibt ein error aufgetreten');
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie`);
+    expect(req.request.method).toBe('GET');
+    req.flush({ message: errorMessage }, { status: 500, statusText: 'Internal Server Error' });
+  });
+
+  it('should update a category', () => {
+    const categoryId = 1;
+    const categoryData = { name: 'Updated Category' };
+    const expectedResponse: iKategorie = { id: 1, parent_id: null, name: 'Updated Category', products: [] };
+
+    service.updateCategory(categoryId, categoryData).subscribe((result) => {
+      expect(result).toEqual([expectedResponse]);
+      expect(snackbarSpy).toHaveBeenCalledWith('Kategorie wurde geändert', 'ok', { duration: 1000 });
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(expectedResponse);
+  });
+
+  it('should handle error when updating a category', () => {
+    const categoryId = 1;
+    const categoryData = { name: 'Updated Category' };
+    const errorMessage = 'Error updating category';
+
+    service.updateCategory(categoryId, categoryData).subscribe((result) => {
+      expect(result).toEqual([]);
+      expect(errorServiceSpy).toHaveBeenCalledWith(errorMessage);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1`);
+    expect(req.request.method).toBe('PUT');
+    req.flush({ message: errorMessage }, { status: 500, statusText: 'Internal Server Error' });
+  });
+
+  it('should delete a category', () => {
+    const categoryId = 1;
+
+    service.deleteCategory(categoryId).subscribe((result) => {
+      expect(result).toEqual([]);
+      expect(snackbarSpy).toHaveBeenCalledWith('Kategorie wurde gelöscht', 'ok', { duration: 1000 });
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(true);
+  });
+
+  it('should handle error when deleting a category', () => {
+    const categoryId = 1;
+    const errorMessage = 'Error deleting category';
+
+    service.deleteCategory(categoryId).subscribe((result) => {
+      expect(result).toEqual([]);
+      expect(errorServiceSpy).toHaveBeenCalledWith(errorMessage);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({ message: errorMessage }, { status: 500, statusText: 'Internal Server Error' });
+  });
+
+  it('should add a product to a category', () => {
+    const categoryId = 1;
+    const productId = 1;
+    const expectedResponse: iKategorie = { id: 1, parent_id: null, name: 'Category 1', products: [] };
+
+    service.addProductToCategory(categoryId, productId).subscribe((result) => {
+      expect(result).toEqual(expectedResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1/products/1`);
+    expect(req.request.method).toBe('POST');
+    req.flush(expectedResponse);
+  });
+
+  it('should remove a product from a category', () => {
+    const categoryId = 1;
+    const productId = 1;
+    const expectedResponse: iKategorie = { id: 1, parent_id: null, name: 'Category 1', products: [] };
+
+    service.removeProductFromCategory(categoryId, productId).subscribe((result) => {
+      expect(result).toEqual(expectedResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}kategorie/1/products/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(expectedResponse);
   });
 });
--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,21 +1,291 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogConfig, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { iUserData } from '../model/iUserData';
+import { JwtModule } from '@auth0/angular-jwt';
+import { By } from '@angular/platform-browser';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let userService: UserService;
+  let dialog: MatDialog;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        JwtModule.forRoot({}),
+        MatDialogModule,
+        MatButtonModule,
+      ],
+      providers: [MatDialog, MatButton]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserComponent]
-    });
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    dialog = TestBed.inject(MatDialog);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component).toBeDefined();
+  });
+
+  describe('ngOnInit', () => {
+    it('should call getUserDetails method', () => {
+      const getUserDetailsSpy = jest.spyOn(userService, 'getUserDetails').mockReturnValue(
+        of({
+          id: 1,
+          vorname: 'John',
+          nachname: 'Doe',
+          email: 'johndoe@example.com',
+          telefon: '123456789',
+          adresse: {
+            strasse: 'Main St',
+            hausnummer: '123',
+            stadt: 'City',
+            postleitzahl: '12345',
+            land: 'Country',
+          },
+        } as iUserData)
+      );
+
+      component.ngOnInit();
+      component.user$.subscribe(() => {})
+
+      expect(getUserDetailsSpy).toHaveBeenCalled();
+    });
+
+    it('should update userForm with received user data', () => {
+      const getUserDetailsSpy = jest.spyOn(userService, 'getUserDetails').mockReturnValue(
+        of({
+          id: 1,
+          vorname: 'John',
+          nachname: 'Doe',
+          email: 'johndoe@example.com',
+          telefon: '123456789',
+          adresse: {
+            strasse: 'Main St',
+            hausnummer: '123',
+            stadt: 'City',
+            postleitzahl: '12345',
+            land: 'Country',
+          },
+        } as iUserData)
+      );
+
+      component.ngOnInit();
+      component.user$.subscribe(() => {})
+
+      expect(getUserDetailsSpy).toHaveBeenCalled();
+      expect(component.userForm.get('vorname')?.value).toEqual('John');
+      expect(component.userForm.get('nachname')?.value).toEqual('Doe');
+      expect(component.userForm.get('email')?.value).toEqual('johndoe@example.com');
+      expect(component.userForm.get('telefon')?.value).toEqual('123456789');
+      expect(component.userForm.get('adresseStrasse')?.value).toEqual('Main St');
+      expect(component.userForm.get('adresseHausnummer')?.value).toEqual('123');
+      expect(component.userForm.get('adresseStadt')?.value).toEqual('City');
+      expect(component.userForm.get('adressePostleitzahl')?.value).toEqual('12345');
+        });
+
+    it('should not update userForm if received user data is null', () => {
+      const getUserDetailsSpy = jest.spyOn(userService, 'getUserDetails').mockReturnValue(  of({
+        id: null,
+        vorname: null,
+        nachname: null,
+      } as unknown as iUserData))
+
+      component.ngOnInit();
+      component.user$.subscribe(() => {})
+      expect(getUserDetailsSpy).toHaveBeenCalled();
+      expect(component.userForm.get('vorname')?.value).toEqual('');
+      expect(component.userForm.get('nachname')?.value).toEqual('');
+      expect(component.userForm.get('email')?.value).toEqual('');
+      expect(component.userForm.get('telefon')?.value).toEqual('');
+      expect(component.userForm.get('adresseStrasse')?.value).toEqual('');
+      expect(component.userForm.get('adresseHausnummer')?.value).toEqual('');
+      expect(component.userForm.get('adresseStadt')?.value).toEqual('');
+      expect(component.userForm.get('adressePostleitzahl')?.value).toEqual('');
+      expect(component.userForm.get('adresseLand')?.value).toEqual('');
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should call updateUser method from userService with updated user data', () => {
+      const getUserDetailsSpy = jest.spyOn(userService, 'getUserDetails').mockReturnValue(
+        of({
+          id: 1,
+          vorname: 'John',
+          nachname: 'Doe',
+          email: 'johndoe@example.com',
+          telefon: '123456789',
+          adresse: {
+            strasse: 'Main St',
+            hausnummer: '123',
+            stadt: 'City',
+            postleitzahl: '12345',
+            land: 'Country',
+          },
+        } as iUserData)
+      );
+      const updateUserSpy = jest.spyOn(userService, 'updateUser').mockImplementation(() => {
+        return null as any
+      })
+      component.ngOnInit();
+      component.user$.subscribe(() => {})
+      component.userid = 1;
+      component.updateUser();
+      component.user$.subscribe(() => {})
+      expect(updateUserSpy).toHaveBeenCalled();
+    });
+
+    it('should refresh the form with the updated user data', () => {
+      const getUserDetailsSpy = jest.spyOn(userService, 'getUserDetails').mockReturnValue(
+        of({
+          id: 1,
+          vorname: 'John',
+          nachname: 'Doe',
+          email: 'johndoe@example.com',
+          telefon: '123456789',
+          adresse: {
+            strasse: 'Main St',
+            hausnummer: '123',
+            stadt: 'City',
+            postleitzahl: '12345',
+            land: 'Country',
+          },
+        } as iUserData)
+      );
+
+      const updateUserSpy = jest.spyOn(userService, 'updateUser').mockReturnValue(of({
+        id: 1,
+        vorname: 'John up',
+        nachname: 'Doe up',
+        email: 'johndoe@example.com up',
+        telefon: '123456789 up',
+        adresse: {
+          strasse: 'Main St up',
+          hausnummer: '123',
+          stadt: 'City',
+          postleitzahl: '12345',
+          land: 'Country',
+        },
+      }))
+
+      component.ngOnInit();
+      component.user$.subscribe(() => {})
+      component.updateUser();
+      component.user$.subscribe(() => {})
+
+      expect(getUserDetailsSpy).toHaveBeenCalled();
+      expect(updateUserSpy).toHaveBeenCalledWith({
+        id: 1,
+        vorname: 'John',
+        nachname: 'Doe',
+        email: 'johndoe@example.com',
+        telefon: '123456789',
+        adresse: {
+          strasse: 'Main St',
+          hausnummer: '123',
+          stadt: 'City',
+          postleitzahl: '12345',
+          land: 'Country',
+        },
+      } as iUserData);
+      expect(component.userForm.get('vorname')?.value).toEqual('John up');
+      expect(component.userForm.get('nachname')?.value).toEqual('Doe up');
+      expect(component.userForm.get('email')?.value).toEqual('johndoe@example.com up');
+      expect(component.userForm.get('telefon')?.value).toEqual('123456789 up');
+      expect(component.userForm.get('adresseStrasse')?.value).toEqual('Main St up');
+      expect(component.userForm.get('adresseHausnummer')?.value).toEqual('123');
+      expect(component.userForm.get('adresseStadt')?.value).toEqual('City');
+      expect(component.userForm.get('adressePostleitzahl')?.value).toEqual('12345');
+      expect(component.userForm.get('adresseLand')?.value).toEqual('Country');
+    });
+  });
+
+  describe('changePassword', () => {
+    it('should open the change password dialog', () => {
+      const openSpy = jest.spyOn(dialog, 'open').mockReturnValueOnce({} as MatDialogRef<ChangePasswordComponent>);
+      const dialogConfig: MatDialogConfig = new MatDialogConfig();
+      dialogConfig.height = '400px';
+      dialogConfig.width = '400px';
+
+      component.changePassword();
+
+      expect(openSpy).toHaveBeenCalledWith(ChangePasswordComponent, dialogConfig);
+    });
+  });
+  it('should call update method on button click', () => {
+    const updateUserSpy = jest.spyOn(component, 'updateUser');
+    component.user$.subscribe(() => {})
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const updateButton = fixture.debugElement.query(
+        By.css('#upuser')
+      );
+
+      updateButton.triggerEventHandler('click', null);
+
+    })
+    expect(updateUserSpy).toHaveBeenCalled();
+  });
+
+  it('should call change password method on button click', async () => {
+
+    const changePasswordSpy = jest.spyOn(component, 'changePassword');
+    const userData: iUserData = {
+      id: 1,
+      vorname: 'John',
+      nachname: 'Doe',
+      email: 'johndoe@example.com',
+      telefon: '123456789',
+      adresse: {
+        strasse: 'Main St',
+        hausnummer: '123',
+        stadt: 'City',
+        postleitzahl: '12345',
+        land: 'Country',
+      },
+    };
+    const getDet =  jest.spyOn(userService, 'getUserDetails').mockReturnValue(
+      of(userData)
+    );
+
+    component.ngOnInit();
+    component.user$.subscribe(() => {
+      return userData;
+    })
+
+    fixture.detectChanges();
+      const changePasswordButton = fixture.debugElement.query(By.css('#cpass'));;
+      console.log(fixture.debugElement.nativeElement)
+      changePasswordButton.triggerEventHandler('click', null);
+
+    expect(getDet).toHaveBeenCalled();
+    expect(changePasswordSpy).toHaveBeenCalled();
   });
 });
+

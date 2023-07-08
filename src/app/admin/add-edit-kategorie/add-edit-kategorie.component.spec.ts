8f93d7b8994ab12e2db422f0773089ff19e5ca6d
Warczachowski <wlodek_w@yahoo.de>
2023-07-08 09:22:54 +0200

    angular update and fix dependency conflicts

--- src/app/admin/add-edit-kategorie/add-edit-kategorie.component.spec.ts
+++ src/app/admin/add-edit-kategorie/add-edit-kategorie.component.spec.ts
@@ -1,21 +1,114 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { AddEditKategorieComponent } from './add-edit-kategorie.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KategorieService } from '../kategories/kategorie.service';
+import { Observable, of } from 'rxjs';
+import { iKategorie } from 'src/app/model/iKategorie';
+import { TransitionCheckState } from '@angular/material/checkbox';
 
 describe('AddEditKategorieComponent', () => {
   let component: AddEditKategorieComponent;
   let fixture: ComponentFixture<AddEditKategorieComponent>;
-
+  let dialogRef: MatDialogRef<AddEditKategorieComponent>;
+  let kategorieService: KategorieService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddEditKategorieComponent]
-    });
+      declarations: [AddEditKategorieComponent],
+      imports: [MatDialogModule, HttpClientTestingModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatSnackBarModule, MatSelectModule, MatInputModule, BrowserAnimationsModule],
+      providers: [  { provide: MatDialogRef, useClass: MatDialogRefMock },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        KategorieService],
+    }).compileComponents();
     fixture = TestBed.createComponent(AddEditKategorieComponent);
     component = fixture.componentInstance;
+    dialogRef = TestBed.inject(MatDialogRef);
+    kategorieService = TestBed.inject(KategorieService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the form with data if provided', () => {
+    const data = {
+      id: 1, parent_id: 2, name: 'Category Name',
+    };
+    component.data = data as iKategorie;
+    component.kategorieForm.patchValue(data);
+    fixture.detectChanges();
+
+    expect(component.kategorieForm.value).toEqual(data);
+  });
+
+  it('should initialize the form with default values if no data is provided', () => {
+    expect(component.kategorieForm.value).toEqual({ id: null, parent_id: null, name: '' });
+  });
+
+  it('should close the dialog on cancel click', () => {
+    jest.spyOn(dialogRef, 'close');
+
+    component.onCancelClick();
+
+    expect(dialogRef.close).toHaveBeenCalled();
+  });
+
+  it('should save the category and close the dialog on save click for new category', fakeAsync( () => {
+    const data = {
+      id: 1, parent_id: 2, name: 'Category Name',
+    };
+    jest.spyOn(dialogRef, 'close');
+    jest.spyOn(kategorieService, 'createCategory').mockReturnValue(of([data as iKategorie]));
+
+    component.kategorieForm.setValue(data);
+    component.onSaveClick();
+  /*  let kategorie$: Observable<any> | undefined;
+    Object.defineProperty(component,  'kategorie$', {
+      get :() => kategorie$,
+      set: (value) => {
+          kategorie$ = value;
+          if(kategorie$) {
+            kategorie$.subscribe(() => {
+              fixture.detectChanges();
+              tick();
+            })
+          }
+      }
+    })*/
+    tick();
+    fixture.detectChanges();
+    console.log(component.kategorieForm)
+    expect(kategorieService.createCategory).toHaveBeenCalledWith(data);
+    expect(dialogRef.close).toHaveBeenCalled();
+  }));
+
+  it('should update the category and close the dialog on save click for existing category', () => {
+    const formData = {
+      id: 1, parent_id: 2, name: 'Category Name',
+
+    };
+    const existingData  = {
+      id: 1, parent_id: 2, name: 'Existing Category',
+
+    };
+    jest.spyOn(dialogRef, 'close');
+    jest.spyOn(kategorieService, 'updateCategory').mockReturnValue(of([formData,existingData] as iKategorie[]));
+    component.data = existingData as iKategorie;
+
+    component.kategorieForm.setValue(formData);
+    component.onSaveClick();
+    expect(kategorieService.updateCategory).toHaveBeenCalledWith(formData.id, formData as iKategorie);
+    expect(dialogRef.close).toHaveBeenCalled();
+  });
 });
+class MatDialogRefMock {
+  close() {}
+}
--- src/app/user/change-password/change-password.component.spec.ts
+++ src/app/user/change-password/change-password.component.spec.ts
@@ -1,21 +1,147 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { ChangePasswordComponent } from './change-password.component';
+import { ErrorService } from 'src/app/error/error.service';
+import { UserService } from '../user.service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { iNewPassword } from 'src/app/model/iNewPassword';
+import {  Observable, of } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ChangePasswordComponent', () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
+  let userService: UserService;
+  let authService: AuthService;
+  let dialogRef: MatDialogRef<ChangePasswordComponent>;
+
+  beforeEach(async () => {
+    const userServiceMock = {
+      changePassword: jest.fn().mockReturnValue(of(1)),
+    };
+
+    const authServiceMock = {
+      getUserId: jest.fn().mockReturnValue(123),
+    };
+
+    const dialogRefMock = {
+      close: jest.fn(),
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [ChangePasswordComponent],
+      providers: [
+        FormBuilder,
+        ErrorService,
+        { provide: UserService, useValue: userServiceMock },
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: MatDialogRef, useValue: dialogRefMock },
+      ],
+      imports: [MatFormFieldModule, MatInputModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule]
+    })
+    .overrideComponent(ChangePasswordComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.OnPush },
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ChangePasswordComponent]
-    });
     fixture = TestBed.createComponent(ChangePasswordComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    authService = TestBed.inject(AuthService);
+    dialogRef = TestBed.inject(MatDialogRef);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form with empty values', () => {
+    const form: FormGroup = component.changePasswordForm;
+    expect(form.get('oldPassword')?.value).toBe('');
+    expect(form.get('newPassword')?.value).toBe('');
+    expect(form.get('confirmPassword')?.value).toBe('');
+  });
+
+  it('should set the password mismatch error when passwords do not match', () => {
+    const form: FormGroup = component.changePasswordForm;
+    form.patchValue({
+      newPassword: 'newPassword',
+      confirmPassword: 'confirmPassword',
+    });
+    expect(form.hasError('mismatch')).toBe(true);
+  });
+
+  it('should not set the password mismatch error when passwords match', () => {
+    const form: FormGroup = component.changePasswordForm;
+    form.patchValue({
+      newPassword: 'newPassword',
+      confirmPassword: 'newPassword',
+    });
+    expect(form.hasError('mismatch')).toBe(false);
+  });
+
+  it('should call changePassword method and close the dialog when form is submitted and valid', fakeAsync(() => {
+    const form: FormGroup = component.changePasswordForm;
+    form.patchValue({
+      oldPassword: 'oldPassword',
+      newPassword: 'newPassword*12',
+      confirmPassword: 'newPassword*12',
+    });
+    form.markAsDirty();
+    console.log(form)
+
+    const pass: iNewPassword = {
+      userid: 123,
+      altPassword: 'oldPassword',
+      newPassword: 'newPassword*12',
+    };
+    const changePasswordSpy = jest.spyOn(userService, 'changePassword').mockReturnValue(of(1));
+    const closeSpy = jest.spyOn(dialogRef, 'close');
+
+    let active$: Observable<any> | undefined;
+    Object.defineProperty(component, 'active$', {
+      get: () => active$,
+      set: (value) => {
+        active$ = value;
+        if (active$) {
+          active$.subscribe(() => {
+            fixture.detectChanges();
+            tick();
+          });
+        }
+      },
+    });
+
+    component.onSubmit();
+
+    component.active$ = of(1);
+
+    tick();
+
+    expect(changePasswordSpy).toHaveBeenCalledWith(pass);
+    expect(closeSpy).toHaveBeenCalled();
+  }));
+  it('should not call changePassword method and close the dialog when form is submitted but invalid', () => {
+    const form: FormGroup = component.changePasswordForm;
+    form.patchValue({
+      oldPassword: '',
+      newPassword: 'newPassword',
+      confirmPassword: 'newPassword',
+    });
+    const changePasswordSpy = jest.spyOn(userService, 'changePassword');
+    const closeSpy = jest.spyOn(dialogRef, 'close');
+
+    component.onSubmit();
+
+
+    expect(changePasswordSpy).not.toHaveBeenCalled();
+    expect(closeSpy).not.toHaveBeenCalled();
   });
 });

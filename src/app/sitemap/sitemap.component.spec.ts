6a3fc044d07c644e8ef875493fc4d057acf15030
Warczachowski <wlodek_w@yahoo.de>
2024-01-12 04:49:03 +0100

    start to work on fix test in angular, ready to update to last version

--- src/app/sitemap/sitemap.component.spec.ts
+++ src/app/sitemap/sitemap.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SitemapComponent } from './sitemap.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { beforeEach } from 'node:test';
+import { ErrorComponent } from '../error/error.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SitemapService } from './sitemap.service';
 
 describe('SitemapComponent', () => {
   let component: SitemapComponent;
   let fixture: ComponentFixture<SitemapComponent>;
-
+  global.fetch = jest.fn();
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SitemapComponent]
+      imports: [SitemapComponent, MatButtonModule, ErrorComponent, MatCardModule],
+      providers: [HttpClientTestingModule, SitemapService],
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SitemapComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

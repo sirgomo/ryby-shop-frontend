1778e60dfe36debcf983b11de19e46a9fc07ad82
Warczachowski <wlodek_w@yahoo.de>
2024-02-10 06:25:27 +0100

    aktion update

--- src/app/model/iAktion.ts
+++ src/app/model/iAktion.ts
@@ -1,8 +1,8 @@
 import { iProduct } from "./iProduct";
 
 export interface iAktion{
-
-  id: number | undefined;
+  id?: number;
+  aktion_key: string;
   produkt: iProduct[];
   startdatum: string;
   enddatum: string;

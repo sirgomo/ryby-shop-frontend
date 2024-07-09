b10bb496d83710e0368373ccac4d08a950e2c4ed
Warczachowski Wlodzimierz <wlodek_w@yahoo.de>
2024-07-09 19:04:22 +0200

    fix paypal loader

--- src/app/card/paypal/paypal.component.ts
+++ src/app/card/paypal/paypal.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -13,7 +13,7 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./paypal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatProgressSpinnerModule, MatButtonModule, MatProgressSpinnerModule]
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatProgressSpinnerModule]
 })
 export class PaypalComponent implements OnInit{
   paypal! :PayPalNamespace | null;
@@ -23,102 +23,104 @@ export class PaypalComponent implements OnInit{
     this.loadPaypal(this.data, this.dialRef);
   }
   async loadPaypal(res: iBestellung, dialRef: MatDialogRef<PaypalComponent>) {
+    if(!window)
+      return;
 
     try {
       this.paypal = await loadScript({ clientId: environment.paypal_client_id, currency: 'EUR'});
-    } catch (err) {
-      console.log('failed to load paypal....')
-    }
 
-    if(this.paypal?.Buttons) {
-      try {
-        this.spinner.set(false);
-        await this.paypal.Buttons({
-          async createOrder() {
-            try {
-              const response = await fetch(`${environment.api}order/create`, {
-                method: 'post',
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(res),
-              })
-              const orderData = await response.json();
-
-              if(orderData.id) {
-                return orderData.id;
-              } else {
-                const errorDetail = orderData?.details?.[0];
-                const errorMessage = errorDetail?`${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`: JSON.stringify(orderData);
-
-                throw new Error(errorMessage);
-              }
-
-            } catch (err) {
-              const tmp = { statusCode: 0, message : ''};
-
-              resultMessage(`Could not initiate PayPal Checkout...<br><br>${err}`);
-            }
-          },
-          async onApprove(data, actions) {
-
+      if(this.paypal?.Buttons) {
+        try {
+          this.spinner.set(false);
+          await this.paypal.Buttons({
+            async createOrder() {
               try {
-                const response = await fetch(`${environment.api}order/capture`, {
-                  method: "POST",
+                const response = await fetch(`${environment.api}order/create`, {
+                  method: 'post',
                   headers: {
                     "Content-Type": "application/json",
                   },
-                  body: JSON.stringify({'orderID': data.orderID, 'bestellung': res})
-                });
-
+                  body: JSON.stringify(res),
+                })
                 const orderData = await response.json();
-
-                // Three cases to handle:
-                //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                //   (2) Other non-recoverable errors -> Show a failure message
-                //   (3) Successful transaction -> Show confirmation or thank you message
-
-                const errorDetail = orderData?.details?.[0];
-
-                if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-                  // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                  // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-                  return actions.restart();
-                } else if (errorDetail) {
-                  // (2) Other non-recoverable errors -> Show a failure message
-                  console.log(errorDetail)
-                  throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
-                } else if (!orderData.purchase_units) {
-                  throw new Error(JSON.stringify(orderData));
+  
+                if(orderData.id) {
+                  return orderData.id;
                 } else {
-                  // (3) Successful transaction -> Show confirmation or thank you message
-                  // Or go to another URL:  actions.redirect('thank_you.html');
-                  dialRef.close('COMPLETED');
-                /*  const transaction =
-                    orderData?.purchase_units?.[0]?.payments?.captures?.[0] ||
-                    orderData?.purchase_units?.[0]?.payments?.authorizations?.[0];
-                  resultMessage(
-                    `Transaction ${transaction.status}: ${transaction.id}<br><br>See console for all available details`,
-                  );
-                  console.log(
-                    "Capture result",
-                    orderData,
-                    JSON.stringify(orderData, null, 2),
-                  );*/
+                  const errorDetail = orderData?.details?.[0];
+                  const errorMessage = errorDetail?`${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`: JSON.stringify(orderData);
+  
+                  throw new Error(errorMessage);
                 }
+  
               } catch (err) {
-                console.log(err);
-                resultMessage(`Sorry, your transaction could not be processed...<br><br>${err}`);
+                const tmp = { statusCode: 0, message : ''};
+  
+                resultMessage(`Could not initiate PayPal Checkout...<br><br>${err}`);
               }
-          },
-          async onShippingChange(data, actions) {
-            resultMessage('Addres wird nicht geeandert');
-          },
-
-        }).render("#paypal-container");
-      } catch (err) {
-        console.log(err)
+            },
+            async onApprove(data, actions) {
+  
+                try {
+                  const response = await fetch(`${environment.api}order/capture`, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({'orderID': data.orderID, 'bestellung': res})
+                  });
+  
+                  const orderData = await response.json();
+  
+                  // Three cases to handle:
+                  //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                  //   (2) Other non-recoverable errors -> Show a failure message
+                  //   (3) Successful transaction -> Show confirmation or thank you message
+  
+                  const errorDetail = orderData?.details?.[0];
+  
+                  if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                    // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                    // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                    return actions.restart();
+                  } else if (errorDetail) {
+                    // (2) Other non-recoverable errors -> Show a failure message
+                    console.log(errorDetail)
+                    throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+                  } else if (!orderData.purchase_units) {
+                    throw new Error(JSON.stringify(orderData));
+                  } else {
+                    // (3) Successful transaction -> Show confirmation or thank you message
+                    // Or go to another URL:  actions.redirect('thank_you.html');
+                    dialRef.close('COMPLETED');
+                  /*  const transaction =
+                      orderData?.purchase_units?.[0]?.payments?.captures?.[0] ||
+                      orderData?.purchase_units?.[0]?.payments?.authorizations?.[0];
+                    resultMessage(
+                      `Transaction ${transaction.status}: ${transaction.id}<br><br>See console for all available details`,
+                    );
+                    console.log(
+                      "Capture result",
+                      orderData,
+                      JSON.stringify(orderData, null, 2),
+                    );*/
+                  }
+                } catch (err) {
+                  console.log(err);
+                  resultMessage(`Sorry, your transaction could not be processed...<br><br>${err}`);
+                }
+            },
+            async onShippingChange(data, actions) {
+              resultMessage('Addres wird nicht geeandert');
+            },
+  
+          }).render("#paypal-container");
+        } catch (err) {
+          console.log(err)
+        }
       }
+    } catch (err) {
+      console.log('failed to load paypal....')
     }
   }
   close() {
